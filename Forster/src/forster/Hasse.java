a688cea6fe2dc5afaece81411c7239cbb2a4ecaf
Gabriela Tourinho <gabrielantourinho@gmail.com>
2016-04-28 22:22:12 -0300

    Fim do programinha. Hasse funcional.

--- Forster/src/forster/Hasse.java
+++ Forster/src/forster/Hasse.java
@@ -14,13 +14,19 @@ import java.util.ArrayList;
 public class Hasse {
     
     private int edges[][];
-    private int topological[];
+    private int topological[], indegree[], S[];
+    private int firstS, lastS, lastT;
     private ArrayList<Integer> subgroups;
 
     public Hasse(ArrayList<Integer> a){
         subgroups = a;
         edges = new int[a.size()][a.size()];
         topological = new int[a.size()];
+        lastT = 0;
+        S = new int[a.size()];
+        firstS = 0;
+        lastS = 0;
+        indegree = new int[a.size()];
     }
     
     public boolean isContained(int sub1, int sub2){
@@ -33,25 +39,66 @@ public class Hasse {
     public int[][] edgeMatrix(){
         
         for (int i=0; i < subgroups.size(); i++){
-            for (int j=0; j < subgroups.size(); j++)
+            for (int j=0; j < subgroups.size(); j++){
                 edges[i][j]=0;
+                indegree[j]=0;
+            }
         }
         
         for (int i=0; i < subgroups.size(); i++){
             for (int j=0; j < subgroups.size(); j++){
-                if (isContained(subgroups.get(i),subgroups.get(j)) && i!=j)
+                if (isContained(subgroups.get(i),subgroups.get(j)) && i!=j){
                     edges[i][j]=1;
+                    indegree[j]++;
+                }
             }
         }
         
         return edges;
     }
     
-    /*public int[] topologicalSort(){
+    public int[] topologicalSort(){
+        for (int i=0; i < subgroups.size(); i++){
+            if (indegree[i]==0){
+                S[lastS]=i;
+                lastS++;
+            }
+        }
         
-    }*/
+        while (firstS!=lastS){
+            topological[lastT]=S[firstS];
+            firstS++;
+            lastT++;
+            
+            for (int j=0; j < subgroups.size(); j++){
+                if (edges[topological[lastT]][j] == 1){
+                    indegree[j]--;
+                }
+                if (edges[topological[lastT]][j]==0){
+                    S[lastS]=j;
+                    lastS++;
+                }
+            }
+        }
+        
+        return topological;
+    }
     
     public int[][] hasseMatrix(){
+        for (int i=lastT-1; i >= 0; i--){
+            int first = -1;
+            
+            for (int j=i-1; j >= 0; j--){
+                if (first < 0){
+                    if (edges[topological[i]][topological[j]] == 1)
+                        first = j;
+                } else {
+                    if (edges[topological[first]][topological[j]] == 1)
+                        edges[topological[i]][topological[j]] = 0;
+                }
+            }
+        }
+        
         return edges;
     }
     
